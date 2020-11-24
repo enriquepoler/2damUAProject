783cc9fb5a840abbdc9eb0645bb9d2bc6cfa34a9
enriquepoler <enrique.poler.perez@gmail.com>
2020-11-24 10:02:02 +0100

    movent carpetes

--- login_jesus/menu.py
+++ login_jesus/menu.py
@@ -1,5 +1,4 @@
 import sys
-
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import (
@@ -9,56 +8,115 @@ from PyQt5.QtWidgets import (
     QLabel,
     QMainWindow,
     QStatusBar,
-    QToolBar,
+    QToolBar
 )
+import cronometro
+import time
+import icons
+import resources
 
-
-# tag::MainWindow[]
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-
-        label = QLabel("Hello!")
+        self.setWindowTitle("Menú")
+        
+        label = QLabel("Benvingut!")
         label.setAlignment(Qt.AlignCenter)
 
         self.setCentralWidget(label)
 
-        toolbar = QToolBar("My main toolbar")
+        toolbar = QToolBar("Toolbar")
         toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
 
-        button_action = QAction(QIcon("bug.png"), "&Your button", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
-        button_action.setCheckable(True)
-        toolbar.addAction(button_action)
-
+        #Botó cronòmetre
+        button_action_cronometro = QAction(QIcon("menu.png"), "&Cronometro", self)
+        button_action_cronometro.setStatusTip("Cronometro")
+        #Conectem la funció cronometroWindow
+        button_action_cronometro.triggered.connect(self.cronometroWindow)
+        
+        self.buttonCronometroCheck = button_action_cronometro
+        self.buttonCronometroCheck.setCheckable(True)
+        
+        toolbar.addAction(button_action_cronometro)
         toolbar.addSeparator()
 
-        button_action2 = QAction(QIcon("bug.png"), "Your &button2", self)
-        button_action2.setStatusTip("This is your button2")
-        button_action2.triggered.connect(self.onMyToolBarButtonClick)
-        button_action2.setCheckable(True)
+        button_action2 = QAction(QIcon("bug.png"), "&Boto2", self)
+        button_action2.setStatusTip("Boto2")
+        button_action2.triggered.connect(self.button2)
+        #button_action2.setCheckable(True)
         toolbar.addAction(button_action2)
 
-        toolbar.addWidget(QLabel("Hello"))
-        toolbar.addWidget(QCheckBox())
+        #self.checkableButton2 = button_action2.setCheckable(True)
+
+        self.buttonFunc2Check = button_action2     
+        self.buttonFunc2Check.setCheckable(True)
+
+        button_action3 = QAction(QIcon("bug.png"), "&Boto3", self)
+        button_action3.setStatusTip("Boto3")
+        button_action3.triggered.connect(self.button3)
+        #button_action3.setCheckable(True)
+        toolbar.addAction(button_action3)
+
+        self.buttonFunc3Check = button_action3 
+        self.buttonFunc3Check.setCheckable(True)
+
+        #toolbar.addWidget(QLabel("Benvingut"))
+        #toolbar.addWidget(QCheckBox())
 
         self.setStatusBar(QStatusBar(self))
 
         menu = self.menuBar()
 
-        file_menu = menu.addMenu("&File")
-        file_menu.addAction(button_action)
+        file_menu = menu.addMenu("&Menu")
+        file_menu.addAction(button_action_cronometro)
+        file_menu.addAction(button_action2)
+        file_menu.addAction(button_action3)
 
-    def onMyToolBarButtonClick(self, s):
+        #Iconos
+
+        '''style = button_action_cronometro.style() # Get the QStyle object from the widget.
+        icon = style.standardIcon(QStyle.SP_MessageBoxCritical)
+        button_action_cronometro.setIcon(icon)'''
+        
+
+    def cronometroWindow(self, s):
         print("click", s)
 
+        #Quan fem click sobre el botó, es seleccionarà
+        self.buttonCronometroCheck.setChecked(True)
 
-# end::MainWindow[]
+        #Comproba si esta seleccionat, si ho està llevarà el que estiga seleccionat
+        if self.buttonFunc2Check.isChecked():
+            self.buttonFunc2Check.setChecked(False)
+        elif self.buttonFunc3Check.isChecked():
+            self.buttonFunc3Check.setChecked(False)
 
+        self.windowCron = cronometro.Window()
+        time.sleep(1)
+
+    def button2(self, s):
+        print("click", s)
+
+        self.buttonFunc2Check.setChecked(True)
+
+        if self.buttonCronometroCheck.isChecked():
+            self.buttonCronometroCheck.setChecked(False)
+        elif self.buttonFunc3Check.isChecked():
+            self.buttonFunc3Check.setChecked(False)
+
+    def button3(self, s):
+        print("click", s)
+
+        self.buttonFunc3Check.setChecked(True)
+
+        if self.buttonCronometroCheck.isChecked():
+            self.buttonCronometroCheck.setChecked(False)
+        elif self.buttonFunc2Check.isChecked():
+            self.buttonFunc2Check.setChecked(False)
+
+        
 
 app = QApplication(sys.argv)
 
