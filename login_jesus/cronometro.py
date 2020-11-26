18c0fbb81f1cd449220f4967937e09f8210f63b3
enriquepoler <enrique.poler.perez@gmail.com>
2020-11-26 09:23:28 +0100

    canviada l'interficie grafica del cronometre

--- login_jesus/cronometro.py
+++ login_jesus/cronometro.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui 
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import * 
+from PyQt5.uic import loadUi
 import sys 
 import decimal
 import os
@@ -11,6 +12,11 @@ import time
 
 #Relative paths
 dirname = os.path.dirname(__file__)
+chronoui = os.path.join(dirname, 'cronometre.ui')
+playIcon = os.path.join(dirname, 'recursos/play.png')
+lapIcon = os.path.join(dirname, 'recursos/lapTimer.png')
+pauseIcon = os.path.join(dirname, 'recursos/pause.png')
+saveIcon = os.path.join(dirname, 'recursos/save.png')
 usersdb = os.path.join(dirname, 'users.db')
   
 class Window(QWidget): 
@@ -18,7 +24,13 @@ class Window(QWidget):
     def __init__(self, user): 
         super().__init__() 
         self.threadpool = QThreadPool()
+        loadUi(chronoui, self)
+        # setting window center on screen
+        qtRectangle = self.frameGeometry()
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        qtRectangle.moveCenter(centerPoint)
         # setting title 
+        self.move(qtRectangle.topLeft())
         self.setWindowTitle("Cronòmetre") 
         #status of the chronometer to swap between stages
         self.status = 0
@@ -29,7 +41,7 @@ class Window(QWidget):
         self.savedLap = True
         self.totalLap = 0
         # setting geometry 
-        self.setGeometry(100, 100, 600, 600) 
+        #self.setGeometry(100, 100, 600, 600) 
         #Saving user information
         self.user = user
         # calling method 
@@ -47,50 +59,53 @@ class Window(QWidget):
         self.count = 0
   
         self.text = (self.count / 10)
-        self.textLaps = ""
+        self.textLap = "0.0 \n0.0 \n0.0\n0.0"
+        self.labelLap.setText(self.textLap)
+        self.staticTextLap = "Fase 1:\nFase 2:\nFase 3:\nTotal volta:"
+        self.staticLap.setText(self.staticTextLap)
         # creating flag 
         self.flag = False
   
-        # creating a label to show the time 
-        self.label = QLabel(self) 
+        # creating a chrono to show the time 
+        #self.chrono = QLabel(self) 
   
-        # setting geometry of label 
-        self.label.setGeometry(75, 100, 450, 70) 
+        # setting geometry of chrono 
+        #self.chrono.setGeometry(75, 100, 450, 70) 
   
-        # adding border to the label 
-        self.label.setStyleSheet("border : 4px solid black;") 
+        # adding border to the chrono 
+        #self.chrono.setStyleSheet("border : 4px solid black;") 
   
-        # setting text to the label 
-        self.label.setText(str(self.count)) 
+        # setting text to the chrono 
+        self.chrono.setText(str(self.count)) 
   
-        # setting font to the label
-        self.label.setFont(QFont('Arial', 15)) 
+        # setting font to the chrono
+        #self.chrono.setFont(QFont('Arial', 15)) 
   
-        # setting alignment to the text of label 
-        self.label.setAlignment(Qt.AlignCenter) 
+        # setting alignment to the text of chrono 
+        #self.chrono.setAlignment(Qt.AlignCenter) 
   
         # creating start button 
-        self.startStopSwitchBtn = QPushButton("Start", self) 
+        #self.startStopSwitchBtn = QPushButton("Start", self) 
   
         # setting geometry to the button 
-        self.startStopSwitchBtn.setGeometry(75, 250, 150, 40) 
-  
+        #self.startStopSwitchBtn.setGeometry(75, 250, 150, 40) 
+        self.startStopSwitchBtn.setIcon(QIcon(playIcon))
         # add action to the method 
         self.startStopSwitchBtn.pressed.connect(self.StartStopSwitch) 
   
         # creating reset button 
-        self.saveLapBtn = QPushButton("Save lap", self) 
+        #self.saveLapBtn = QPushButton("Save lap", self) 
   
         # setting geometry to the button 
-        self.saveLapBtn.setGeometry(375, 250, 150, 40) 
-  
+        #self.saveLapBtn.setGeometry(375, 250, 150, 40) 
+        self.saveLapBtn.setIcon(QIcon(saveIcon))
         # add action to the method 
         self.saveLapBtn.pressed.connect(self.saveLap)
         self.saveLapBtn.setEnabled(False) 
   
         # creating a editable layout 
-        anotations = QLineEdit()
-        anotations.move(50, 50)
+        #anotations = QLineEdit()
+        #anotations.move(50, 50)
         # creating a timer object 
         timer = QTimer(self) 
   
@@ -101,19 +116,19 @@ class Window(QWidget):
         timer.start(100)
 
         # Create a label to show de laps
-        self.labelLap = QLabel(self) 
+        #self.labelLap = QLabel(self) 
   
         # setting geometry of label 
-        self.labelLap.setGeometry(225, 500, 150, 100) 
-        self.labelLap.setStyleSheet("border : 4px solid black;") 
+        #self.labelLap.setGeometry(225, 500, 150, 100) 
+        #self.labelLap.setStyleSheet("border : 4px solid black;") 
         # adding border to the label 
         #self.labelLap.setStyleSheet("border : 4px solid black;") 
   
         # setting font to the label
-        self.labelLap.setFont(QFont('Arial', 15)) 
+        #self.labelLap.setFont(QFont('Arial', 15)) 
   
         # setting alignment to the text of label 
-        self.labelLap.setAlignment(Qt.AlignCenter) 
+        #self.labelLap.setAlignment(Qt.AlignCenter) 
 
   
     # method called by timer 
@@ -129,7 +144,7 @@ class Window(QWidget):
         self.text = str(self.count / 10) 
 
         # showing text 
-        self.label.setText(self.text) 
+        self.chrono.setText(self.text) 
   
     def StartStopSwitch(self): 
         #Switch with if/else
@@ -140,30 +155,30 @@ class Window(QWidget):
             #start the timer with the flag
             #and switch to lap1 with status 1 and change textbutton to "Lap 1"
             self.savedLap = True
-            self.textLaps = ""
+            self.textLap = "0.0\n0.0\n0.0\n0.0"
             self.lap1 = 0.0
             self.lap2 = 0.0
             self.lap3 = 0.0
-            self.labelLap.setText(self.textLaps)
+            self.labelLap.setText(self.textLap)
             self.flag = True
             self.status = 1
-            self.startStopSwitchBtn.setText("Lap 1")
+            self.startStopSwitchBtn.setIcon(QIcon(lapIcon))
         elif(self.status == 1):
             #If status is 1 and the button get pressed, swap the text of te button to next lap (Lap 2)
             #update the label and show the lap1 (partitional time) and status is 2
-            self.startStopSwitchBtn.setText("Lap 2")
+            #self.startStopSwitchBtn.setText("Lap 2")
             self.status = 2
             self.lap1 = self.text
-            self.textLaps += "Lap 1: {} \n".format(self.lap1)
-            self.labelLap.setText(self.textLaps)  
+            self.textLap = str(self.lap1) + "\n0.0\n0.0\n0.0"
+            self.labelLap.setText(self.textLap)  
         elif(self.status == 2):
             #If status is 2 and the button get pressed, swap the text of te button to next lap (Lap 3/Stop)
             #update the label and show the lap1 and 2 (partitional time) and status is 3
-            self.startStopSwitchBtn.setText("Lap 3/Stop")
+            self.startStopSwitchBtn.setIcon(QIcon(pauseIcon))
             self.status = 3
             self.lap2 = float(self.text) - float(self.lap1)
-            self.textLaps += "Lap 2: {} \n".format(round(self.lap2, 1)) 
-            self.labelLap.setText(self.textLaps)
+            self.textLap = str(self.lap1) + "\n" + str(round(self.lap2, 1)) + "\n0.0\n0.0" 
+            self.labelLap.setText(self.textLap)
         elif(self.status == 3):
             #Disable start/stob button for 2 seconds to prevent start another lap without save the prev lap
             self.threadpool.start(self.chrono_stopper)
@@ -172,21 +187,21 @@ class Window(QWidget):
             # reset the timer to 0, status is 0, the flag is false to stop the timer
             self.savedLap = False
             self.flag = False
-            self.startStopSwitchBtn.setText("Start")
+            self.startStopSwitchBtn.setIcon(QIcon(playIcon))
             self.lap3 = float(self.text) - float(self.lap2) - float(self.lap1)
-            self.textLaps += "Lap 3: {} \n".format(round(self.lap3, 1))
-            self.labelLap.setText(self.textLaps)
+            self.textLap = str(self.lap1) + "\n" + str(round(self.lap2, 1)) + "\n" + str(round(self.lap3, 1)) + "\n" + self.text
+            self.labelLap.setText(self.textLap)
              # reseeting the count 
             self.count = 0
             self.status = 0
-            self.totalLap = self.text
+            #self.totalLap = self.text
             # setting text to label 
-            self.label.setText(str(self.count))
-            self.textLaps += "Total lap: {} \n".format(self.totalLap)
+            self.chrono.setText(str(self.count))
+            #self.textLap += "Total lap: {} \n".format(self.totalLap)
             # setting save lap button enabled
             self.saveLapBtn.setEnabled(True)
             
-            self.labelLap.setText(self.textLaps)
+            self.labelLap.setText(self.textLap)
   
     def saveLap(self): 
         if(self.lap1 == 0.0 and self.lap2 == 0.0 and self.lap3 == 0.0):
@@ -200,30 +215,29 @@ class Window(QWidget):
             connection.commit()
             
             # FER MULTIFIL PER PODER ACTUALITZAR EL TEXT PASAT UNS SEGONS SENSE CONGELAR L'APP
-            self.lap_saver_thread = Lap_saver_thread(self.labelLap, self.textLaps)
+            self.lap_saver_thread = Lap_saver_thread(self.labelLap, self.textLap)
             #TO-DO: Desactivar directament el boto de guardar volta quan no siga posible guardar-la!
             self.saveLapBtn.setEnabled(False)
             # AFEGIR EDIT TEXT PER A LES ANOTACIONS!
             self.threadpool.start(self.lap_saver_thread)
-                                    
-            print("Volta guardada")
+                                                
             connection.close()
 
 class Lap_saver_thread(QRunnable):
     '''
     Worker thread
     '''
-    def __init__(self, textWidget, textLaps):
+    def __init__(self, textWidget, labelLap):
         super(Lap_saver_thread, self).__init__()
         self.textWidget = textWidget
-        self.textLaps = textLaps
+        self.labelLap = labelLap
 
     @pyqtSlot()
     def run(self):
         
-        self.textWidget.setText("Volta guardada!")
+        self.textWidget.setText("Vuelta guardada!")
         time.sleep(2)
-        self.textWidget.setText(self.textLaps)
+        self.textWidget.setText(self.labelLap)
 
 class Chrono_stopper(QRunnable):
     '''
