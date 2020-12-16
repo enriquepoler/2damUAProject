1c7bd3a6b489a4752d7a950be4aa19bdf71674aa
JesusHuertasMancebo <45229081+JesusHuertasMancebo@users.noreply.github.com>
2020-12-16 13:19:31 +0100

    Update grafica_pacients.py

--- login_jesus/grafica_pacients.py
+++ login_jesus/grafica_pacients.py
@@ -19,18 +19,14 @@ class MainWindow(QtWidgets.QMainWindow):
         loadUi(grafica_pacients_ui, self)
 
         #Perfil Medit
-        self.velocitatMedit = 8
-        self.forçaMedit = 62
+        self.velocitatMedit = 2.8
 
         self.velocitat = [0,self.velocitatMedit]
-        self.força = [0,self.forçaMedit]
 
         #Perfil Òptim
-        self.velocitatOptim = 6
-        self.forçaOptim = 47
+        self.velocitatOptim = 3.4
 
         self.velocitatDos = [0,self.velocitatOptim]
-        self.forçaDos = [0,self.forçaOptim]
 
         # Per a canviar el color de fondo
         self.graphWidget.setBackground("w")
@@ -38,45 +34,38 @@ class MainWindow(QtWidgets.QMainWindow):
         #Afegim titol i color
         self.graphWidget.setTitle("Gràfica Pacients", color='k', size="15pt")
 
-        #Afegim Força (N/kg) i Velocitat (m/s)
-        self.graphWidget.setLabel('left', "<span style=\"color:black;fontsize:30px\">Força (N/Kg)</span>")
-        self.graphWidget.setLabel('bottom', "<span style=\"color:black;fontsize:30px\">Velocitat (m/s)</span>")
+        #Velocitat (m/s)
+        self.graphWidget.setLabel('left', "<span style=\"color:black;fontsize:30px\">Velocitat (m/s)</span>")
 
         #Afegim una cuadrícula a la gràfica
         self.graphWidget.showGrid(x=True, y=True)
 
         #Afegim el rango
-        self.graphWidget.setXRange(0, 10, padding=0)
+        #self.graphWidget.setXRange(0, 10, padding=0)
         self.graphWidget.setYRange(0, 100, padding=0)
 
         #Color roig de la línia de la gràfica
         pen = pg.mkPen(color=(255, 0, 0))
 
         #El self.plot() del perfil medit el tenim repetir perquè sino no apareix
-        self.plot(self.velocitat, self.força, "Perfil Medit", "b")
-        self.plot(self.velocitatDos, self.forçaDos, "Perfil Òptim", "r")
-        self.plot(self.velocitat, self.força, "Perfil Medit", "b")
+        self.plot(self.velocitat, "Perfil Medit", "b")
+        self.plot(self.velocitatDos, "Perfil Òptim", "r")
+        self.plot(self.velocitat, "Perfil Medit", "b")
 
-        f_0_Label=self.f0Label.text()
         v_0_Label=self.v0Label.text()
 
-        print("f0:", self.forçaMedit, "N/Kg")
         print("v0:", self.velocitatMedit, "m/s")
 
         #Soles es mostra el medit a la part de baix
-        self.f0Label.setText(str(self.forçaMedit) + " N/Kg")
         self.v0Label.setText(str(self.velocitatMedit) + " m/s")
-
-        print("f0 label:", f_0_Label)
-        print("v0 label:", v_0_Label)
 
         self.show()
 
     #Mètode que li passem la velocitat la força el nom de la llegenda i el color
-    def plot(self, x, y, plotname, color):
+    def plot(self, x, plotname, color):
         pen = pg.mkPen(color=color)
         self.graphWidget.plot(
-            x, y, name=plotname, pen=pen, symbol="+", symbolSize=20,
+            x, name=plotname, pen=pen, symbol="+", symbolSize=20,
             symbolBrush=(color)
         
         )
