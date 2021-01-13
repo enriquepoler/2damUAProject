5777fa4aac6247c49ce2b3260d806ece1a245ec7
JesusHuertasMancebo <45229081+JesusHuertasMancebo@users.noreply.github.com>
2021-01-13 16:04:34 +0100

    Update sqliteConsulter.py

--- login_jesus/sqliteConsulter.py
+++ login_jesus/sqliteConsulter.py
@@ -134,7 +134,7 @@ class SQLite_consulter:
         cursor = self.connection.cursor()
         cursor.execute(
             "SELECT totalTime FROM laps WHERE name = ? and surname = ?", (patient[0], patient[1]))
-        rows = cursor.fetchone()
+        rows = cursor.fetchall()
 
         return rows
     
@@ -145,7 +145,7 @@ class SQLite_consulter:
         cursor = self.connection.cursor()
         cursor.execute(
             "SELECT lap1 FROM laps WHERE name = ? and surname = ?", (patient[0], patient[1]))
-        rows = cursor.fetchone()
+        rows = cursor.fetchall()
 
         return rows
 
@@ -156,7 +156,7 @@ class SQLite_consulter:
         cursor = self.connection.cursor()
         cursor.execute(
             "SELECT lap2 FROM laps WHERE name = ? and surname = ?", (patient[0], patient[1]))
-        rows = cursor.fetchone()
+        rows = cursor.fetchall()
 
         return rows
 
@@ -167,7 +167,7 @@ class SQLite_consulter:
         cursor = self.connection.cursor()
         cursor.execute(
             "SELECT lap3 FROM laps WHERE name = ? and surname = ?", (patient[0], patient[1]))
-        rows = cursor.fetchone()
+        rows = cursor.fetchall()
 
         return rows
 
